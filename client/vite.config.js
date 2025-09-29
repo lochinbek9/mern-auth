67cd5749a72014a78047253102790c78cac96305
Lochinbek Dehkonov <103257271+lochinbek9@users.noreply.github.com>
2025-09-29 12:33:03 +0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://auth-6fw7.onrender.com/',
         secure: false,
       },
     },
