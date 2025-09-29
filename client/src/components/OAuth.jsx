89e91be4bc7c71118dd8a039d90d95efedb6062d
Lochinbek Dehkonov <103257271+lochinbek9@users.noreply.github.com>
2025-09-29 12:49:20 +0500

    Update OAuth.jsx

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -3,17 +3,22 @@ import { app } from '../firebase';
 import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/userSlice';
 import { useNavigate } from 'react-router-dom';
+import { API } from '../api'; // API faylidan URL olish
 
 export default function OAuth() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
 
+
       const result = await signInWithPopup(auth, provider);
-      const res = await fetch('/api/auth/google', {
+
+     
+      const res = await fetch(API.googleSignin, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -23,22 +28,31 @@ export default function OAuth() {
           email: result.user.email,
           photo: result.user.photoURL,
         }),
+        credentials: 'include', 
       });
+
       const data = await res.json();
-      console.log(data);
+
+      if (!res.ok) {
+        console.error('Google login failed:', data);
+        return;
+      }
+
+      console.log('Google login response:', data);
       dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
-      console.log('could not login with google', error);
+      console.log('Could not login with Google:', error);
     }
   };
+
   return (
     <button
       type='button'
       onClick={handleGoogleClick}
       className='bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95'
     >
-      Continue with google
+      Continue with Google
     </button>
   );
 }
